a2f287b6aa7a7610778fde4a9745f7af6d8314d6
CarolineHussey <66087850+CarolineHussey@users.noreply.github.com>
2024-07-30 01:01:39 +0100

    Banner rendering converted to PHP

--- themes/fictional-block-theme/our-blocks/banner.php
+++ themes/fictional-block-theme/our-blocks/banner.php
@@ -1,1 +1,7 @@
-Our Blocks banner.php with render callback
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image:
+            url('/wp-content/themes/fictional-block-theme/images/library-hero.jpg')"></div>
+  <div class="page-banner__content container t-center c-white">
+    <?php echo $content; ?>
+  </div>
+</div>
