0766dc7b38b87b919ca57214bebb134e1473e9cd
CarolineHussey <66087850+CarolineHussey@users.noreply.github.com>
2024-09-22 00:21:43 +0100

    enhancements to about us and events sections
    new images and content

--- themes/fictional-university-theme/page.php
+++ themes/fictional-university-theme/page.php
@@ -5,53 +5,75 @@
         the_post(); 
         pageBanner();
         ?>
-        
 
-            <div class="container container--narrow page-section">
 
-            <?php 
-            $theParent = wp_get_post_parent_id(get_the_ID());
-            if ($theParent) { ?>
+<div class="container container--narrow page-section">
 
-            <div class="metabox metabox--position-up metabox--with-home-link">
-                <p>
-                <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent)?> </a> <span class="metabox__main"><?php echo the_title() ?></span>
-                </p>
-            </div>
+  <?php 
+    $theParent = wp_get_post_parent_id(get_the_ID());
+    if ($theParent) { ?>
 
-            <?php }
+  <div class="metabox metabox--position-up metabox--with-home-link">
+    <p>
+      <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home"
+          aria-hidden="true"></i> Back to <?php echo get_the_title($theParent)?> </a> <span
+        class="metabox__main"><?php echo the_title() ?></span>
+    </p>
+  </div>
+
+  <?php }
 
             ?>
 
-            <?php
+  <?php
 
-            $hasChildren = get_pages(array(
-                'child_of' => get_the_ID()
-            ));
+    $hasChildren = get_pages(array(
+        'child_of' => get_the_ID()
+    ));
 
-            if ($theParent or $hasChildren) { ?>
-            <div class="page-links">
-                <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent) ?></a></h2>
-                <ul class="min-list">
-                <?php
-                if ($theParent) {
-                    $childrenOf = $theParent;
-                } else {
-                    $childrenOf = get_the_ID();
-                }
-                    wp_list_pages(array(
-                        'title_li' => NULL,
-                        'child_of' => $childrenOf,
-                        'sort_column' => 'menu_order'
-                    )) ?>
-                </ul>
-            </div>
-            <?php } ?>
+    if ($theParent or $hasChildren) { ?>
+  <div class="page-links">
+    <h2 class="page-links__title"><a
+        href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent) ?></a></h2>
+    <ul class="min-list">
+      <?php
+        if ($theParent) {
+            $childrenOf = $theParent;
+        } else {
+            $childrenOf = get_the_ID();
+        }
+            wp_list_pages(array(
+                'title_li' => NULL,
+                'child_of' => $childrenOf,
+                'sort_column' => 'menu_order'
+            )) ?>
+    </ul>
+  </div>
+  <?php } ?>
 
-            <div class="generic-content">
-                <?php the_content() ?>
-            </div>
-            </div>
+  <div class="generic-content">
+
+    <?php the_content(); 
+        
+        $sanitizeEasterEgg = sanitize_text_field(get_query_var('easterEgg'));
+        $sanitizeColor = sanitize_text_field(get_query_var('color'));?>
+    <h3>Query Vars Test</h3>
+    <form method="get">
+      <input name="easterEgg" placeholder="Easter Egg"><br />
+      <input name="color" placeholder="Color"><br />
+      <button>Submit</button>
+    </form>
+    <?php 
+        
+        if($sanitizeEasterEgg == 'duck' AND $sanitizeColor == 'blue') {
+            echo '<p style="padding-top: 20px">You found a blue easter egg!! Happy hunting :)</p>';   
+        }
+
+        ?>
+
+
+  </div>
+</div>
 <?php }
 
 get_footer();
